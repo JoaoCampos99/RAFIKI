6aac580c8c177240a52f5794de58634561809f02
JoaoCampos99 <j_henrique_campos@hotmail.com>
2018-12-07 20:28:09 +0000

    bootstrap project

--- app/src/main.js
+++ app/src/main.js
@@ -2,11 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import bootstrap from "./assets/bootstrap.min.css";
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  bootstrap,
   render: h => h(App)
 }).$mount("#app");
