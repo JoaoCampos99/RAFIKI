c094c0c9641c554e8c4f5cd64491dfce554edc7f
JoaoCampos99 <j_henrique_campos@hotmail.com>
2018-12-23 18:47:54 +0000

    login update

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -160,7 +160,7 @@ export default {
       }
     },
     login() {
-      console.log(this.users)
+      console.log(this.users);
       let id = this.users.filter(user => {
         if (
           user.email == this.inputEmail &&
