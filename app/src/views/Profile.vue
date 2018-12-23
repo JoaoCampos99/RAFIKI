1961ad1414999094acb6a1d68eabc7a00f86d355
JoaoCampos99 <j_henrique_campos@hotmail.com>
2018-12-23 18:24:06 +0000

    profile very pretty

--- app/src/views/Profile.vue
+++ app/src/views/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container" style="margin-top:80px">
+  <div class="container" style="margin-top:80px;margin-bottom:80px">
         <div class="list-group col-md-3" id="nav">
           <router-link :to="{name:'AboutMe'}" :class="{'list-group-item list-group-item-action':true, 'nav-link':true}">About Me</router-link>
                     <router-link :to="{name:'MyThreads'}" :class="{'list-group-item list-group-item-action':true,'nav-link':true}">My Threads</router-link>
