aea0c20bb3e34f77c099241ea437d1b2c507a37f
JoaoCampos99 <j_henrique_campos@hotmail.com>
2018-12-23 18:02:11 +0000

    button group update

--- app/src/views/Profile.vue
+++ app/src/views/Profile.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="container">
-    <div>Isto é o perfil do {{$route.params.userid}}</div>
-        <div class="list-group col-md-3">
-          <router-link :to="{name:'AboutMe'}" :class="{'list-group-item list-group-item-action':true, 'active':true}">About Me</router-link>
-                    <router-link :to="{name:'MyThreads'}" :class="{'list-group-item list-group-item-action':true, }">My Threads</router-link>
-          <router-link :to="{name:'MyGroups'}" :class="{'list-group-item list-group-item-action':true,}">My Groups</router-link>
-          <router-link :to="{name:'MyBadges'}" :class="{'list-group-item list-group-item-action':true, }">Badges</router-link>
-          <router-link :to="{name:'MyRankings'}" :class="{'list-group-item list-group-item-action':true, }">Rankings</router-link>
+  <div class="container" style="margin-top:80px">
+        <div class="list-group col-md-3" id="nav">
+          <router-link :to="{name:'AboutMe'}" :class="{'list-group-item list-group-item-action':true, 'nav-link':true}">About Me</router-link>
+                    <router-link :to="{name:'MyThreads'}" :class="{'list-group-item list-group-item-action':true,'nav-link':true}">My Threads</router-link>
+          <router-link :to="{name:'MyGroups'}" :class="{'list-group-item list-group-item-action':true,'nav-link':true}">My Groups</router-link>
+          <router-link :to="{name:'MyBadges'}" :class="{'list-group-item list-group-item-action':true, 'nav-link':true}">Badges</router-link>
+          <router-link :to="{name:'MyRankings'}" :class="{'list-group-item list-group-item-action':true, 'nav-link':true}">Rankings</router-link>
     <router-view></router-view>
 </div>
   </div>
@@ -19,3 +18,10 @@ export default {
   }
 };
 </script>
+<style>
+#nav a.router-link-exact-active {
+  color: #212529;
+  background-color: #007bff;
+  border-color: #007bff;
+}
+</style>
