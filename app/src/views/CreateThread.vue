75d5614d042aa0e9274722a05f808885a44af0ed
JoaoCampos99 <j_henrique_campos@hotmail.com>
2019-01-08 23:16:01 +0000

    quill.register nao funciona (tentar perceber depois)

--- app/src/views/CreateThread.vue
+++ app/src/views/CreateThread.vue
@@ -1,58 +1,57 @@
 <template>
   <div>
-    <div class="container">    <h1>Create Thread</h1>
-    <div class="row">
-      <div class="col-sm-8">
-        <div class="form-group">
-          <label for="inputTitle">Title</label>
-          <input type="text"
-            class="form-control" id="inputTitle" >
+    <div class="container">
+      <h1>Create Thread</h1>
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="form-group">
+            <label for="inputTitle">Title</label>
+            <input type="text" class="form-control" id="inputTitle">
+          </div>
+          <div class="form-group">
+            <label for="inputContent">Describe your question</label>
+            <vue-editor v-model="content" :editorToolbar="customToolbar"></vue-editor>
+          </div>
+          <div class="form-group">
+            <label for="inputTags">Related Tags</label>npm
+            <input type="text" class="form-control" id="inputTags">
+          </div>
         </div>
-        <div class="form-group">
-          <label for="inputContent">Describe your question</label>
-            <vue-editor v-model="content" :editorToolbar="customToolbar"  ></vue-editor>
+        <div class="col-sm-4">
+          <div class="card text-white bg-primary mb-3">
+            <div class="card-header">Featured</div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p
+                class="card-text"
+              >With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="inputTags">Related Tags</label>npm 
-          <input type="text"
-            class="form-control" id="inputTags">
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="card text-white bg-primary mb-3">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
       </div>
     </div>
-</div>
   </div>
 </template>
 
 <script>
-import { VueEditor,Quill} from "vue2-editor";
+import { VueEditor, Quill } from "vue2-editor";
 export default {
   components: {
-      VueEditor,
-   },
-   data(){
-     return{
-       content:"",
-        customToolbar: [
-            ['bold', 'italic', 'underline'],
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            ['image', 'code-block']
-          ]
-     }
-   },
-   updated(){
-     console.log(this.content)
-   },
+    VueEditor
+  },
+  data() {
+    return {
+      content: "",
+      customToolbar: [
+        ["bold", "italic", "underline"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["image", "code-block"]
+      ]
+    };
+  },
+  updated() {
+    console.log(this.content);
+  }
 };
 </script>
