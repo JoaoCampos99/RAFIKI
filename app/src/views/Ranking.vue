91c4e823305669d091e3236cf1c858da9f9f6986
JoaoCampos99 <j_henrique_campos@hotmail.com>
2019-01-10 22:30:18 +0000

    ranking bonitinho

--- app/src/views/Ranking.vue
+++ app/src/views/Ranking.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
     <div class="table-responsive">
-      <table class="table table-striped table-hover table-bordered table-light">
+      <table class="table table-striped table-hover table-bordered">
         <thead class="thead-dark">
           <tr>
             <th>#</th>
@@ -11,7 +11,11 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(user) in orderedUsers" v-bind:key="user.id">
+          <tr
+            v-for="(user) in orderedUsers"
+            v-bind:key="user.id"
+            v-bind:class="{'table-active':(user.id==userid)}"
+          >
             <td scope="row">{{user.rank[0]}}</td>
             <td>{{user.name}}</td>
             <td>{{user.level}}</td>
@@ -27,11 +31,15 @@
 export default {
   data() {
     return {
-      users: []
+      users: [],
+      userid: 0
     };
   },
   created() {
     this.users = this.$store.getters.getUsers;
+    if (this.$store.getters.getloginID != 0) {
+      this.userid = this.$store.getters.getloginID;
+    }
   },
   computed: {
     orderedUsers() {
@@ -48,3 +56,10 @@ export default {
   }
 };
 </script>
+<style>
+.table-active,
+.table-active > td,
+.table-active > th {
+  background-color: rgb(255, 223, 135);
+}
+</style>
