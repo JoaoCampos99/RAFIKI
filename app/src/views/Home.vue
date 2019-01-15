57f5ceb710bddb30b4479e249c01f69ccb116a37
JoaoCampos99 <j_henrique_campos@hotmail.com>
2019-01-15 19:38:39 +0000

    Hot topics_feito para já

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,11 +2,23 @@
   <div id="containerHome" class="container">
     <h1>Isto é uma página home</h1>
     <div class="row">
-      <div class="col-md-10">
+      <div class="col-md-9">
         <Catalog/>
       </div>
-      <div class="col-md-2" style="padding: 0 0 0 5">
-        <h3>Para aqui vêm "Notas"</h3>
+      <div class="col-md-3" style="padding: 0 0 0 5">
+        <h4 style="padding: 0 0 0 5; padding-inline-start: 20px">Hot Topic</h4>
+        <div class="fitText">
+          <div style="padding: 0 0 0 5; padding-inline-start: 20px" class="list-group">
+            <a
+              style="width:100%"
+              class="list-group-item"
+              v-for="thread in hotThreads"
+              v-bind:key="thread.id"
+              @click="goToThread(thread.id)"
+            >{{thread.title}}</a>
+          </div>
+        </div>
+        <h3>Para aqui vêm Related</h3>
         <div class="fitText">
           <ul style="padding: 0 0 0 5; padding-inline-start: 20px">
             <li style="margin-bottom: 20px; margin-bottom: 20px; ">
@@ -26,6 +38,30 @@ import Catalog from "@/components/Catalog.vue";
 export default {
   components: {
     Catalog
+  },
+  data() {
+    return {
+      hotThreads: []
+    };
+  },
+  created() {
+    this.hotThreads = this.$store.getters.getThreads.sort((a, b) => {
+      if (a.views > b.views) return -1;
+      if (a.views > b.views) return 1;
+      else return 0;
+    });
+    console.log(this.hotThreads);
+    this.hotThreads.length = 5;
+  },
+  methods: {
+    goToThread(id) {
+      this.$router.push({
+        name: "thread",
+        params: {
+          threadid: id
+        }
+      });
+    }
   }
 };
 </script>
