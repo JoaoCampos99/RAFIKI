c7fb238bfc6426f5305b5253a94446619d7d7a8d
JoaoCampos99 <j_henrique_campos@hotmail.com>
2019-01-20 21:45:01 +0000

    HotTopics feito

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -6,15 +6,31 @@
         <Catalog/>
       </div>
       <div class="col-md-3" style="padding: 0 0 0 5">
-        <h4 style="padding: 0 0 0 5; padding-inline-start: 20px">Most viewed Topics</h4>
+        <h4 style="padding: 0 0 0 5; padding-inline-start: 20px">Hot Topics</h4>
         <div class="fitText">
           <div style="padding: 0 0 0 5; padding-inline-start: 20px" class="list-group">
+            <!-- Assim só aparece 5 threads -->
             <a
               style="width:100%"
               class="list-group-item"
-              v-for="thread in viewedThreads"
+              v-for="(thread,index) in hotTopics"
               v-bind:key="thread.id"
               @click="goToThread(thread.id)"
+              v-if="index<5"
+            >{{thread.title}}</a>
+          </div>
+        </div>
+        <h4 style="padding: 0 0 0 5; padding-inline-start: 20px">Most viewed Topics</h4>
+        <div class="fitText">
+          <div style="padding: 0 0 0 5; padding-inline-start: 20px" class="list-group">
+            <!-- Assim só aparece 5 threads -->
+            <a
+              style="width:100%"
+              class="list-group-item"
+              v-for="(thread,index) in viewedThreads"
+              v-bind:key="thread.id"
+              @click="goToThread(thread.id)"
+              v-if="index<5"
             >{{thread.title}}</a>
           </div>
         </div>
@@ -37,16 +53,46 @@ export default {
   },
   data() {
     return {
-      viewedThreads: []
+      viewedThreads: [],
+      hotTopics: []
     };
   },
   created() {
     this.viewedThreads = this.$store.getters.getThreads.sort((a, b) => {
       if (a.views > b.views) return -1;
-      if (a.views > b.views) return 1;
+      if (a.views < b.views) return 1;
       else return 0;
     });
     // this.viewedThreads.length = 5;
+    this.hotTopics = this.$store.getters.getThreads.map(thread => {
+      let newObj = {
+        id: thread.id,
+        title: thread.title,
+        points: thread.upvotes + thread.views
+      };
+      return newObj;
+    });
+    console.log("Antes de receber pontos por comentarios" + this.hotTopics);
+    let answers = this.$store.getters.getAnswers;
+    let comments = this.$store.getters.getComments;
+    for (let i = 0; i < this.hotTopics.length; i++) {
+      for (let j = 0; j < answers.length; j++) {
+        if (this.hotTopics[i].id == answers[j].idThread) {
+          for (let k = 0; k < comments.length; k++) {
+            if (answers[j].id == comments[k].idAnswer) {
+              this.hotTopics[i].points += 1;
+            }
+          }
+          this.hotTopics[i].points += 1;
+        }
+      }
+    }
+    console.log(this.hotTopics);
+    this.hotTopics = this.hotTopics.sort((a, b) => {
+      if (a.points > b.points) return -1;
+      if (a.points < b.points) return 1;
+      else return 0;
+    });
   },
   methods: {
     goToThread(id) {
